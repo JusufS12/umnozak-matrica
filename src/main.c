5c4b44fd3a2e4a147777b12ac92a20b35ee3e327
Josip Sikirić <jurematic2006@gmail.com>
2023-10-17 18:31:06 +0200

    added support for negative numbers

--- src/main.c
+++ src/main.c
@@ -10,16 +10,20 @@ int unosBroja() {
     int rezultat = 0;
     fgets(broj, sizeof broj, stdin);
     for (i = 0; broj[i] != '\0'; ++i) {
-        if ('0' <= broj[i] && broj[i] <= '9' || '-') {
+        if ('0' <= broj[i] && broj[i] <= '9') {
             rezultat *= 10;
             rezultat += broj[i] - '0';
         }
     }
+	if (broj[0] == '-') {
+		rezultat = -rezultat;
+	}
     return rezultat;
 }
 
 void grafickiPrikaz(size_t red, size_t stupac) {
 	char *point = "V\n";
+	puts("\n");
 	for (size_t i = 0; i < 3; i++) {
 		if (red == i) {
 			switch (stupac)
@@ -41,7 +45,6 @@ void grafickiPrikaz(size_t red, size_t stupac) {
 		}
 		puts("[] [] []");
 	}
-	puts("\n");
 }
 
 void popuniMatricu(int mat[3][3]) {
@@ -54,6 +57,12 @@ void popuniMatricu(int mat[3][3]) {
 	}
 }
 
+void prikazMatrice(int mat[3][3]) {
+	puts("\n");
+	printf("[%d] [%d] [%d]\n[%d] [%d] [%d]\n[%d] [%d] [%d]\n", mat[0][0], mat[0][1], mat[0][2], mat[1][0], mat[1][1], mat[1][2], mat[2][0], mat[2][1], mat[2][2]);
+	puts("\n");
+}
+
 int main(int argc, char *argv[]) {
 	int matrica1[3][3];
 	int matrica2[3][3];
@@ -63,6 +72,9 @@ int main(int argc, char *argv[]) {
 
 	puts("Unesi drugu matricu:\r\n");
 	popuniMatricu(matrica2);
+
+	prikazMatrice(matrica1);
+	prikazMatrice(matrica2);
 
 	return 0;
 }
