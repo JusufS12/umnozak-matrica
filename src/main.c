e89f364834f76c8a5ba8b99ab9d843910b890872
Josip Sikirić <jurematic2006@gmail.com>
2023-10-17 17:16:39 +0200

    graficki prikaz popunjavanja matrice

--- src/main.c
+++ src/main.c
@@ -2,9 +2,41 @@
 #include <stdlib.h>
 
 
+void grafickiPrikaz(size_t index) {
+	char *point = "V\n";
+	for (size_t i = 3; i < 10; i += 3) {
+		if (index + 1 <= i && index + 1 > i - 3) {
+			switch (i - index)
+			{
+			case 2:
+				printf("%s", point);
+				break;
+			case 1:
+				printf("   %s", point);
+				break;
+			case 0:
+				printf("      %s", point);
+				break;
+			default:
+			puts("Greska!");
+				break;
+			}
+			
+		}
+		puts("[] [] []");
+	}
+}
+
+int popuniMatricu() {
+	int mat[3][3];
+	grafickiPrikaz(7);
+	return mat;
+}
+
 int main(int argc, char *argv[]) {
-	
-	printf("Hello world!");
-	
+	puts("Unesi prvu matricu:\r\n");
+	int matrica1 = popuniMatricu();
+	puts("Unesi drugu matricu:\r\n");
+	int matrica2 = popuniMatricu();
 	return 0;
 }
